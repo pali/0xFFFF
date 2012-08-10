b142205f1c0f0a1202c882ff1994a4d3255c28bf
Pali Rohár <pali.rohar@gmail.com>
2012-08-10 12:14:05 +0200

    Remove old main.h code

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "main.h"
+//#include "main.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
