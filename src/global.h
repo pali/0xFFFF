4ab76a51127b549a96a8a5119f60af432b94684a
Pali Rohár <pali.rohar@gmail.com>
2012-08-10 21:48:17 +0200

    global: Fix ERROR_INFO_STR

--- src/global.h
+++ src/global.h
@@ -12,7 +12,7 @@ extern int verbose;
 
 #define VERBOSE(...) do { if ( verbose ) { fprintf(stderr, __VA_ARGS__); } } while (0)
 #define WARNING(...) do { fprintf(stderr, "Warning: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); } while (0)
-#define ERROR_INFO_STR(str, ...) do { if ( str[0] ) fprintf(stderr, "Error: %s: ", str); else fprintf(stderr, "Error: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); } while (0)
+#define ERROR_INFO_STR(str, ...) do { if ( str[0] ) fprintf(stderr, "Error: %s: ", str); else fprintf(stderr, "Error: "); fprintf(stderr, __VA_ARGS__); if ( errno ) fprintf(stderr, ": %s\n", strerror(errno)); else fprintf(stderr, "\n"); } while (0)
 #define ERROR_STR(str, ...) do { errno = 0; ERROR_INFO_STR(str, __VA_ARGS__); } while (0)
 #define ERROR_INFO(...) do { ERROR_INFO_STR("", __VA_ARGS__); } while (0)
 #define ERROR(...) do { ERROR_STR("", __VA_ARGS__); } while (0)
